14c57e69b32b1de7e1e94b1fb4a7c58f27b5bc94
Vinícius Magalhães Batista <63367957+Magavinicius@users.noreply.github.com>
2022-06-02 20:13:50 -0300

    update de template
    melhoria de texto

--- jogo/Forca.py
+++ jogo/Forca.py
@@ -1,5 +1,6 @@
-def jogar():
-    import random
+import random
+
+def jogar():  
 
     print("****************************")
     print("Bem vindo ao jogo da Forca")
@@ -7,8 +8,8 @@ def jogar():
 
 
     print("Adivinhe a Fruta\n")
-
-    #pegando as frutas dentro de palavras
+    
+    #add chamada de arquivo
     arquivo = open(r"C:\Users\vinic\Documents\MeusProjetos\Jogo-em-Python\jogo\palavras.txt","r")
     palavras = []
     
@@ -28,33 +29,68 @@ def jogar():
     acertou = False
     erros = 0
 
+
     print(letras_acertadas)
     
-    #Loop de tentativa
+    #loop de tentativas
     while(not enforcou and not acertou):
 
-        chute = input("Qual letra?")
-        chute = chute.strip().upper()
-        
-        if(chute in palavra_secreta):
-
-            index = 0
-            for letra in palavra_secreta:
-                if(chute == letra):
-                    letras_acertadas[index] = letra
-                index += 1
-        else:
-             erros += 1 
-        enforcou = erros == 6
-        acertou = "_" not in letras_acertadas
-        print(letras_acertadas)
+            chute = input("Qual letra?")
+            chute = chute.strip().upper()
+            
+            if(chute in palavra_secreta):
+    
+                index = 0
+                for letra in palavra_secreta:
+                    if(chute == letra):
+                        letras_acertadas[index] = letra
+                    index += 1
+            else:
+                erros += 1 
+            enforcou = erros == 6
+            acertou = "_" not in letras_acertadas
+            print(letras_acertadas)
 
     if(acertou):
-        print("Você Ganhou!")
+        print(imprime_mensagem_vencedor)
     else:
-        print("Você morreu!")
+        print(imprime_mensagem_perdedor)
             
 
 
 if(__name__ == "__main__"):
     jogar()
+
+def imprime_mensagem_perdedor(palavra_secreta):
+    print("Puxa, você foi enforcado!")
+    print("A palavra era {}".format(palavra_secreta))
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
+
+def imprime_mensagem_vencedor():
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
